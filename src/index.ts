3dc18802909a185a2deb90163a7cc1aa9a067097
bitsofjp <jim.layacan@gmail.com>
2025-10-21 21:17:04 +0800

    feat: initial project setup with Express, TypeScript, ESLint and Prettier

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// index.js
+
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ?? "9001";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  console.log("Response sent")
+});
+
+app.listen(port, () => {
+  console.log(`This app listens on port ${port}`)
+})
